09c842742a497d5d60c86be7360c4f0f7a7a81b6
Zack Scholl <zack.scholl@gmail.com>
2017-03-19 08:32:47 -0600

    Add better CLI support

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"net/http"
 	"os"
@@ -12,69 +11,145 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/jcelliott/lumber"
+	"gopkg.in/urfave/cli.v1"
 )
 
 // Testing
 // curl -H "Content-Type: application/json" -X POST -d '{"bucket":"food","keystore":{"username":"xyz","password":"xyz"}}' 'http://zack:123@localhost:8080/'
 
+var version string
 var port, dbpath string
 var compressOn, verbose bool
 var log *lumber.ConsoleLogger
 
 func main() {
-	flag.BoolVar(&verbose, "verbose", false, "verbosity")
-	flag.BoolVar(&compressOn, "compress", false, "use compression")
-	flag.StringVar(&dbpath, "db", path.Join(".", "dbs"), "path to the database")
-	flag.StringVar(&port, "port", "8080", "port to listen on")
-	flag.Parse()
+	// flag.BoolVar(&verbose, "verbose", false, "verbosity")
+	// flag.BoolVar(&compressOn, "compress", false, "use compression")
+	// flag.StringVar(&dbpath, "db", path.Join(".", "dbs"), "path to the database")
+	// flag.StringVar(&port, "port", "8080", "port to listen on")
+	// flag.Parse()
+	app := cli.NewApp()
+	app.Name = "boltdb-server"
+	app.Usage = "fancy server for connecting to a BoltDB keystore"
+	app.Version = version
+	app.Compiled = time.Now()
+	app.Action = func(c *cli.Context) error {
+		dbpath = c.GlobalString("db")
+		compressOn = c.GlobalBool("compress")
+		verbose = c.GlobalBool("debug")
+		port = c.GlobalString("port")
+		os.MkdirAll(dbpath, 0755)
 
-	os.MkdirAll(dbpath, 0755)
+		if verbose {
+			log = lumber.NewConsoleLogger(lumber.TRACE)
+		} else {
+			log = lumber.NewConsoleLogger(lumber.WARN)
+		}
 
-	if verbose {
-		log = lumber.NewConsoleLogger(lumber.TRACE)
-	} else {
-		log = lumber.NewConsoleLogger(lumber.WARN)
-	}
+		startTime := time.Now()
 
-	startTime := time.Now()
+		gin.SetMode(gin.ReleaseMode)
+		r := gin.Default()
+		r.GET("/v1/api", func(c *gin.Context) {
+			c.String(200, `
 
-	gin.SetMode(gin.ReleaseMode)
-	r := gin.Default()
-	r.GET("/v1/api", func(c *gin.Context) {
-		c.String(200, `
+				// Get map of buckets and the number of keys in each
+				GET /v1/db/<db>/stats
 
-/v1/db/:dbname/stats // Get map of buckets and the number of keys in each
-/v1/db/:dbname/buckets // Get list of all buckets
-/v1/db/:dbname/bucket/:bucket/numkeys  // Get all keys and values from a bucket (no parameters)
+				// Get list of all buckets
+				GET /v1/db/<db>/buckets
 
-`)
-	})
-	r.GET("/v1/uptime", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"uptime": time.Since(startTime).String(),
+				// Get all keys and values from a bucket
+				GET /v1/db/<db>/bucket/<bucket>/numkeys
+
+				// Get all keys and values from a bucket
+				GET /v1/db/<db>/bucket/<bucket>/all
+
+				// Get all keys and values specified by ?keys=key1,key2 or by JSON
+				GET /v1/db/<db>/bucket/<bucket>/some
+
+				// Delete and return first n keys
+				GET /v1/db/<db>/bucket/<bucket>/pop?n=X
+
+				// Get all keys in a bucket
+				GET /v1/db/<db>/bucket/<bucket>/keys", handleGetKeys)
+
+				// Return boolean of whether it has key
+				GET /v1/db/<db>/bucket/<bucket>/haskey/<key>
+
+				// Return boolean of whether any buckets contain any keys specified by JSON
+				GET /v1/db/<db>/haskeys
+
+				// Delete database file
+				DELETE /v1/db/<db>
+
+				// Delete bucket
+				DELETE /v1/db/<db>/bucket/<bucket>
+
+				// Delete keys, where keys are specified by JSON []string
+				DELETE /v1/db/<db>/bucket/<bucket>/keys
+
+				// Updates a database with keystore specified by JSON
+				POST /v1/db/<db>/bucket/<bucket>/update
+
+				// Move keys, with buckets and keys specified by JSON
+				POST /v1/db/<db>/move
+
+				// Create buckets specified by JSON
+				POST /v1/db/<db>/create
+
+	`)
 		})
-	})
-	r.GET("/v1/db/:dbname/stats", handleGetDBStats)                  // Get map of buckets and the number of keys in each
-	r.GET("/v1/db/:dbname/buckets", handleGetBuckets)                // Get list of all buckets
-	r.GET("/v1/db/:dbname/bucket/:bucket/numkeys", handleGetNumKeys) // Get all keys and values from a bucket (no parameters)
-	r.GET("/v1/db/:dbname/bucket/:bucket/all", handleGet)            // Get all keys and values from a bucket (no parameters)
-	r.GET("/v1/db/:dbname/bucket/:bucket/some", handleGet)           // Get all keys and values specified by ?keys=key1,key2 or by JSON
-	r.GET("/v1/db/:dbname/bucket/:bucket/pop", handlePop)            // Delete and return first n keys + values, where n specified by ?n=100
-	r.GET("/v1/db/:dbname/bucket/:bucket/keys", handleGetKeys)       // Get all keys in a bucket (no parameters)
-	r.GET("/v1/db/:dbname/bucket/:bucket/haskey/:key", handleHasKey) // Return boolean of whether it has key
-	r.GET("/v1/db/:dbname/haskeys", handleHasKeys)                   // Return boolean of whether any of the buckets contain the keys
-	// TODO: r.GET("/v1/db/:dbname/bucket/:bucket/data", getDataArchive)   // Creates archive with keys as filenames and values as contents, returns archive
+		r.GET("/v1/uptime", func(c *gin.Context) {
+			c.JSON(200, gin.H{
+				"uptime": time.Since(startTime).String(),
+			})
+		})
+		r.GET("/v1/db/:dbname/stats", handleGetDBStats)                  // Get map of buckets and the number of keys in each
+		r.GET("/v1/db/:dbname/buckets", handleGetBuckets)                // Get list of all buckets
+		r.GET("/v1/db/:dbname/bucket/:bucket/numkeys", handleGetNumKeys) // Get all keys and values from a bucket (no parameters)
+		r.GET("/v1/db/:dbname/bucket/:bucket/all", handleGet)            // Get all keys and values from a bucket (no parameters)
+		r.GET("/v1/db/:dbname/bucket/:bucket/some", handleGet)           // Get all keys and values specified by ?keys=key1,key2 or by JSON
+		r.GET("/v1/db/:dbname/bucket/:bucket/pop", handlePop)            // Delete and return first n keys + values, where n specified by ?n=100
+		r.GET("/v1/db/:dbname/bucket/:bucket/keys", handleGetKeys)       // Get all keys in a bucket (no parameters)
+		r.GET("/v1/db/:dbname/bucket/:bucket/haskey/:key", handleHasKey) // Return boolean of whether it has key
+		r.GET("/v1/db/:dbname/haskeys", handleHasKeys)                   // Return boolean of whether any of the buckets contain the keys
+		// TODO: r.GET("/v1/db/:dbname/bucket/:bucket/data", getDataArchive)   // Creates archive with keys as filenames and values as contents, returns archive
 
-	r.DELETE("/v1/db/:dbname", handleDeleteDatabase)                 // Delete database file (no parameters)
-	r.DELETE("/v1/db/:dbname/bucket/:bucket", handleDeleteBucket)    // Delete bucket (no parameters)
-	r.DELETE("/v1/db/:dbname/bucket/:bucket/keys", handleDeleteKeys) // Delete keys, where keys are specified by JSON []string
-	//
-	r.POST("/v1/db/:dbname/bucket/:bucket/update", handleUpdate) // Updates a database with keystore specified by JSON
-	r.POST("/v1/db/:dbname/move", handleMove)                    // Move keys, with buckets and keys specified by JSON
-	r.POST("/v1/db/:dbname/create", handleCreateDB)              // Move keys, with buckets and keys specified by JSON
+		r.DELETE("/v1/db/:dbname", handleDeleteDatabase)                 // Delete database file (no parameters)
+		r.DELETE("/v1/db/:dbname/bucket/:bucket", handleDeleteBucket)    // Delete bucket (no parameters)
+		r.DELETE("/v1/db/:dbname/bucket/:bucket/keys", handleDeleteKeys) // Delete keys, where keys are specified by JSON []string
+		//
+		r.POST("/v1/db/:dbname/bucket/:bucket/update", handleUpdate) // Updates a database with keystore specified by JSON
+		r.POST("/v1/db/:dbname/move", handleMove)                    // Move keys, with buckets and keys specified by JSON
+		r.POST("/v1/db/:dbname/create", handleCreateDB)              // Move keys, with buckets and keys specified by JSON
 
-	fmt.Printf("Listening on 0.0.0.0:%s\n", port)
-	r.Run(":" + port) // listen and serve on 0.0.0.0:8080
+		fmt.Printf("Running. See 0.0.0.0:%s/v1/api for API information\n", port)
+		r.Run(":" + port) // listen and serve on 0.0.0.0:8080
+		return nil
+	}
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:  "port, p",
+			Value: "8080",
+			Usage: "port to use to listen",
+		},
+		cli.StringFlag{
+			Name:  "db",
+			Value: path.Join(".", "dbs"),
+			Usage: "path to use for storing databases",
+		},
+		cli.BoolFlag{
+			Name:  "compress, c",
+			Usage: "turn on compression",
+		},
+		cli.BoolFlag{
+			Name:  "debug",
+			Usage: "turn on debug mode",
+		},
+	}
+app.Run(os.Args)
+
 }
 
 func handleHasKeys(c *gin.Context) {
